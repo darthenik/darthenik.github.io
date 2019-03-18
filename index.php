3378a169678579618be9a62d905b0b7d58f3d284
enk <enk@trikster-kab11.miac.local>
2019-03-18 19:37:53 +0300

    change index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>A page</title>
+</head>
+
+<body>
+   <p> Hello world, hello, hello freaking wold, automate as Henry Ford bequeathed to us</p>
+</body>
+</html>
