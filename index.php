441dbdaa348305f7cf1aa9609c14e3c9be562630
enk <enk@trikster-kab11.miac.local>
2019-03-18 21:12:04 +0300

    change php

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
 <?php
-    $test = 'Hello World';
+    phpinfo();
 ?>
 
-<!doctype html>
-<html>
-<head>
-    <title>A page</title>
-</head>
-
-<body>
-   <p> Hello world, hello, hello freaking wold, automate as Henry Ford bequeathed to us</p>
-</body>
-</html>
